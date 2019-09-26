76a51dc80d492ae3bf7c177200987c7ccfe08af5
Eivind Fålun <eivind.falun@gmail.com>
2019-09-26 14:23:07 +0200

    Db connection up and running on port 27017

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose')
+const PORT = 4000;
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/journal', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); // port 27017, use journal
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established success")
+})
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
