e11aae2dde66cd708e7ec06fd4e5955c53a77e39
DidrikSG <didrik@galteland.com>
2019-10-02 18:11:29 +0200

    Update gitignore and add route to server.js

--- api/server.js
+++ api/server.js
@@ -1,23 +1,32 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+const app = express();
+
 const PORT = 4000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://mongodb:27017/journal', {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true
 }); // port 27017, use journal
 const connection = mongoose.connection;
 
-connection.once('open', function() {
+connection.once('open', function () {
     console.log("MongoDB database connection established success")
-})
+});
 
-app.listen(PORT, function() {
+
+// Use routes
+app.use('/api/users', require('./routes/users'));
+
+
+app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
 });
