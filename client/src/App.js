be16e44e1b9ac65787282f4bb9d558f79f848d1a
dennisjl <dennisjianbin@gmail.com>
2019-10-08 13:44:17 +0200

    refactors to match styleguide

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import LoginScreen from "./components/loginscreen/loginscreen";
-import CaseScreen from './components/casescreen/casescreen.js';
+import CaseScreen from "./components/casescreen/CaseScreen.jsx";
 
 class App extends Component{
 
@@ -8,7 +8,7 @@ class App extends Component{
         return(
             <div>
                 <div className="container">
-                    <CaseScreen/>         
+                    <CaseScreen />         
                     <h1>Hello World {this.props.name}</h1>
                     
                 </div>
--- client/src/components/casescreen/CaseScreen.jsx
+++ client/src/components/casescreen/CaseScreen.jsx
@@ -14,18 +14,20 @@ class CaseScreen extends React.Component{
                     <div class="elementsingrid">
                         
                         <div className="gericanr">Gericanr:</div> 
-                        <div className="gericainput"> <input className="inputboxer" type="text" size="35"></input></div>
+                        <div className="gericainput"> 
+                            <input className="inputboxer" type="text" size="35"></input>
+                        </div>
                         <div className="prio"> Prio:</div> 
                         <div className="priograd">1,2,3,4</div>
                         <div className="henvendtfra"> Henvendelse fra: </div> 
-                        <div className="sted"> <input className="inputboxer" type="text" size="35"></input> </div>              
+                        <div className="sted"> 
+                            <input className="inputboxer" type="text" size="35"></input> 
+                        </div>              
                         <div className="aldersgruppe"> Aldersgruppe:</div>
                         <div className="barnvoksen">Barn/Voksen</div>
                         <div className="beskrivelse"> Beskrivelse</div>
                         <div className="beskrivelseboks"> 
-                            <textarea className="inputboxer" rows="3" cols="33">
-                                
-                            </textarea> 
+                            <textarea className="inputboxer" rows="3" cols="33"></textarea> 
                         </div>
                 
                     </div>
