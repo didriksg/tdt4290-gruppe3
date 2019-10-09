fefcc1650216158ac96668433d5a19d6d976802f
dennisjl <dennisjianbin@gmail.com>
2019-10-09 18:10:40 +0200

    modifies casescreen to display components

--- client/src/components/casescreen/CaseScreen.jsx
+++ client/src/components/casescreen/CaseScreen.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./casescreen.css";
-import RadioButtons from "./RadioButtons.jsx"
-
+import RadioButtons from "./RadioButtons.jsx";
+import BarnVoksenSelect from "./BarnVoksenSelect.jsx";
+import DateFields from "./DateFields.jsx";
+import BydelSelect from "./BydelSelect.jsx";
 
 
 class CaseScreen extends React.Component{
@@ -16,19 +18,23 @@ class CaseScreen extends React.Component{
 
                     <div className="elementsingrid">
                         
+                        
                         <div className="gericanr">Gericanr:</div> 
                         <div className="gericainput"> 
                             <input 
                                 className="inputboxer" 
                                 type="number"
                             >
-
                             </input>
                         </div>
-                        <div className="prio"> Prio:</div> 
+
+
+                        <div className="prio"> Prioritet:</div> 
                         <div className="priograd">
                             <RadioButtons />
                         </div>
+
+
                         <div className="henvendtfra"> Henvendelse fra: </div> 
                         <div className="sted"> 
                             <input 
@@ -39,19 +45,67 @@ class CaseScreen extends React.Component{
                                 
                                 </input> 
                         </div>              
+
+
                         <div className="aldersgruppe"> Aldersgruppe:</div>
-                        <div className="barnvoksen">Barn/Voksen</div>
+                        <div className="barnvoksen">
+                            <BarnVoksenSelect />
+                        </div>
+
+
+                        <div className="registreringsdato">Registreringsdato:</div>
+                        <div className="oppstartsdato">Oppstartsdato:</div>
+                        <div className="dato">
+                            <DateFields />
+                        </div>
+
+
+                        <div className="bydel">Bydel:</div>
+                        <div className="bydelbox"> 
+                            <BydelSelect />
+                        </div>
+
+
+                        <div className="kategori"> Kategori</div>
+                        <div className="kategorifelt">
+                            <input
+                                className="inputboxer"
+                                type="text"
+                                size="40.99999"
+                                >
+                            </input>
+                        </div>
+
+
                         <div className="beskrivelse"> Beskrivelse</div>
                         <div className="beskrivelseboks"> 
                             <textarea 
                                 className="inputboxer" 
-                                rows="3" 
+                                rows="6" 
                                 cols="43"
-                            >
-                                
-                                </textarea> 
+                            >                                
+                            </textarea> 
                         </div>
-                
+
+
+                        <div className="avbrytknapp">
+                            <form className="forma">
+                                <button className="abutton" type="submit">Avbryt</button>
+                            </form>
+                        </div>
+
+                        <div className="viktigknapp">
+                            <form className="formb">
+                                <button className="mbutton" type="submit">Marker viktig</button>
+                            </form>
+                        </div>
+
+                        <div className="lagnysakknapp">
+                            <form className="formc">
+                                <button className="lbutton" type="submit">Lag ny sak</button>
+                            </form>
+                        </div>
+
                     </div>
         
                 </div>
