67aa60dac935570fc96cbe9ec8b1145bc8bd6592
Unknown <kiddid96@gmail.com>
2019-10-12 20:12:06 +0200

    Try to connect login module to backend

--- client/src/components/loginscreen/LoginScreen.jsx
+++ client/src/components/loginscreen/LoginScreen.jsx
@@ -1,51 +1,75 @@
 import React from "react";
 import "./LoginScreen.css";
-
+import {connect} from 'react-redux';
+import {login} from '../../actions/authActions'
 
 class LoginScreen extends React.Component{
+    state = {
+        email: '',
+        password: '',
+    };
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        error: PropTypes.object.isRequired,
+        login: PropTypes.func. isRequired,
+    };
+
+    onLoginButtonClick = e => {
+        e.preventDefault();
+        const {email, password} = this.state;
+
+        const user = {
+            email,
+            password,
+        };
+
+        // Attempt to login.
+        this.props.login(user);
+    };
 
     render(){
         return(
             <div className="logincontainer">
-                
+
                 <div className="loginelements">
-                    
+
                     <div className="logo">
-                        <img 
-                            src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Trondheim_komm.svg" 
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Trondheim_komm.svg"
                             width="70" height="80"
                             alt="Trondheim kommune logo"
                         />
                     </div>
 
                     <h1>Enhet for Ergoterapitjeneste</h1>
-                    
+
                     <div className="inputfelter">
                         <form>
-                            <input 
-                                className="usernamebox" 
-                                type="text" 
-                                size="42" 
+                            <input
+                                className="usernamebox"
+                                type="text"
+                                size="42"
                                 placeholder="Brukernavn"
                             >
 
                             </input>
 
-                            <input 
-                                className="usernamebox" 
-                                type="password" 
-                                size="42" 
+                            <input
+                                className="usernamebox"
+                                type="password"
+                                size="42"
                                 placeholder="Passord">
 
                             </input>
 
                         </form>
-                        
+
                     </div>
 
                     <div className="loginbutton">
                         <form>
-                        <button type="submit">Login</button>
+                        <button type="submit" onClick={this.onLoginButtonClick}>Login</button>
                         </form>
                     </div>
 
@@ -56,4 +80,12 @@ class LoginScreen extends React.Component{
 
 }
 
-export default LoginScreen;
+const mapStateToProps = state => ({
+   isAuthenticated: state.auth.isAuthenticated,
+   error: state.error,
+});
+
+export default connect(
+    mapStateToProps,
+    {register},
+)(LoginScreen);
