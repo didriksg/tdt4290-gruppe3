53580c5fa573432038592f7237ff05e3bd8a8721
Unknown <kiddid96@gmail.com>
2019-10-13 15:42:07 +0200

    Add case actions, reducer and constants

---
+++ client/src/actions/caseActions.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import {ADD_CASE, CASES_LOADING, CASES_LOADED, CASE_UPDATING, CASE_UPDATED} from "./constants";
+import {returnErrors} from "./errorActions";
+import {tokenConfig} from "./authActions";
+
+
+const connectionString = 'http://localhost:4000';
+
+export const addCase = c => {
+    return (dispatch, getState) => {
+        const body = JSON.stringify(c);
+        axios
+            .post(`${connectionString}/api/case/add`, body, tokenConfig(getState))
+            .then(res => dispatch({
+                type: ADD_CASE,
+                payload: res.data
+            }))
+            .catch(err => dispatch(returnErrors(err.response.data, err.response.status))
+            );
+    };
+};
+
+export const updateCase = c => {
+    return (dispatch, getState) => {
+        dispatch({type: CASE_UPDATING});
+
+        const body = JSON.stringify(c);
+        axios
+            .put(`${connectionString}/api/case/update/${id}`, body, tokenConfig(getState))
+            .then(res => dispatch({
+                type: CASE_UPDATED,
+                payload: res.data
+            }))
+            .catch(err => dispatch(returnErrors(err.response.data, err.response.status))
+            );
+    };
+};
+
+export const getCases = (state) => {
+    return (dispatch, getState) => {
+        dispatch({type: CASES_LOADING});
+        const body = JSON.stringify(state);
+        axios
+            .get(`${connectionString}/api/case/list`, body, tokenConfig(getState))
+            .then(res => dispatch({
+                type: CASES_LOADED,
+            }))
+            .catch(err => dispatch(returnErrors(err.response.data, err.response.status))
+            );
+    };
+};
+
+export const getCasesById = (id) => {
+    return (dispatch, getState) => {
+        axios
+            .get(`${connectionString}/api/case/${id}`, body, tokenConfig(getState))
+            .then(res => dispatch({
+                type: CASES_LOADING,
+            }))
+            .catch(err => dispatch(returnErrors(err.response.data, err.response.status))
+            );
+    };
+}
