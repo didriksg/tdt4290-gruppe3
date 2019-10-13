911f93e29c6de43ad01175c559e5bb0a7bc7eb93
theodor <theodolh@stud.ntnu.no>
2019-10-13 11:59:56 +0200

    added dialog functionality

--- client/src/components/profilescreen/ProfileScreen.js
+++ client/src/components/profilescreen/ProfileScreen.js
@@ -1,15 +1,30 @@
 import React from "react";
-import {AppBar, Toolbar, Grid, Button, Divider, Typography} from '@material-ui/core'
+import {AppBar, Toolbar, Grid, Button, Divider, Typography, Card, Paper} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
     
     
-      
+      //wish to display this list-values
       function createData(name, district, group, access){
         return {name, district, group, access}
       }
+
+
+      function checkPassword(p1, p2){
+          while(p1 != p2){
+
+            print("Passordene er ikke like. Prøv på nytt")
+        }
+
+      }
       
       
       const useStyles = makeStyles({
@@ -25,13 +40,33 @@ import ListItemText from '@material-ui/core/ListItemText';
             display: 'inline',
           },
       });
+
+
+    const style = {
+        
+        buttons: { marginTop: 10, marginBottom: 10 },
+        
+      }
       
+
     
     
     export default function ProfileScreen() {
       const classes = useStyles();
+
+
+
+    //dialog functionality: "change password"
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+    }
         
       
+
     
       return (
         <div>
@@ -163,12 +198,51 @@ import ListItemText from '@material-ui/core/ListItemText';
         <Grid container>
           <Grid item sm={12}>
             <Grid container justify="center" spacing={2}>
+        
               <Grid item>
               <Button variant="outlined" color="primary">Returner til Min Side</Button>
               </Grid>
-              <Grid item>
-              <Button variant="outlined" color="primary">Endre Passord</Button>
-              </Grid>
+              
+            <Grid item>     
+              <Button variant="outlined" color="primary" onClick={handleClickOpen} style={style.buttons}>Endre Passord</Button>
+            </Grid>
+                        <Dialog open={open} onClose={handleClose}>
+                            <DialogTitle> Endre passord</DialogTitle>
+                            <DialogContent dividers>
+                                <DialogContentText>
+                                    Skriv inn et nytt passord:
+                                </DialogContentText>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    //label="Passord:"
+                                    type="password"
+                                    
+                                />
+                                <DialogContentText>
+                                    Gjenta passord:
+                                </DialogContentText>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    //label="Passord:"
+                                    type="password"
+                                    
+                                />
+                            </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleClose} color="primary">
+                                        Tilbake
+                                    </Button>
+                                    <Button onClick={handleClose} color="primary">
+                                        Endre
+                                    </Button>
+                                </DialogActions>
+                        </Dialog>
+                
+              
             </Grid>
           </Grid>
         </Grid>
