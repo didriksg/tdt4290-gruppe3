d9ae1b3907311c3281a3165d65d31aa8de23e0bf
Thea Tokstad <33182871+theatok@users.noreply.github.com>
2019-10-16 11:38:30 +0200

    Feat/redux logger (#16)
    * adds loginscreen.
    
    * removes unused index.js file
    
    * removes unused  index.js
    
    * Deletes package-lock.js
    
    * Create user model and route
    
    * Update gitignore and add route to server.js
    
    * Add user authentication and registration with a web token
    
    * Fix backend for user creation, login and authentication
    
    * Removal of unused packages.
    
    * Change to fit style
    
    * temporary save
    
    * Cleanup and refactor for new style guide
    
    * Add case model
    
    * Add case route. 
    
    Adding a case, updating a case, and getting a case by id is now possible.
    
    * Update db connection to work with default.json
    
    * Refactor default.json
    
    * Refactoring
    
    * Forgot to commit search on gericaNumber functionality..
    
    * Add sorting functionality and some minor refactoring
    
    * Add JSON file describing allowed categories.
    
    * Feat/routing (#14)
    
    * Adds routing. Changes to fit style.
    
    * forgot to delete code
    
    * changes to fit style
    
    * Add case sorting
    
    * Better extract data from request body.
    
    * Fix name of wrong package in package.json
    
    * Adds cleaner style to boxes
    
    * Update stuff to work with config file + add packges
    
    * refactors to match styleguide
    
    * refactors to match styleguide
    
    * Start implementing dummy data
    
    * Move logic to controllers instead of directly in route
    
    * Update package.json
    
    * Fix some errors, but still some problems.
    
    * Fix dummy data generation
    
    * Adds radiobuttons, modifies casescreen to match
    
    * adds components used in casescreen
    
    * modifies casescreen to display components
    
    * Started on homescreen
    
    * Homescreen with routing functionality done (frontend)
    
    * Adjusts the fields on the new case screen. Modifies different components as well a buxfixes
    
    * minor adjustment
    
    * Added some styling with cards on the components
    
    * package-lock issues
    
    * changes package.json files
    
    * added route for new case
    
    * Add redux stuff
    
    * Temporarily ghettofix lack of proxy server
    
    * Add login action
    
    * Try to connect login module to backend
    
    * Import correctly
    
    * Add babel thingy
    
    * Add working login.
    
    * Prepare for cases
    
    * Forgot to add
    
    * Remove logging
    
    * Add case actions, reducer and constants
    
    * Update API to support new actions
    
    * Code refactor
    
    * Refactor to fit style guide
    
    * Update API to support ES6
    
    * Remove random '1'
    
    * Changed up the routers by accident.
    
    * Feat/overview board (#15)
    
    * adds table over all referrals
    
    * adds css
    
    * deleted unused code
    
    * merge conflict
    
    * deletes package-lock
    
    * changes in the table for alle saker
    
    * Add ES6 support for dummy data gen
    
    * adds redux logger
    
    * Fix ES6 refactor error

---
+++ client/src/actions/errorActions.js
@@ -0,0 +1,16 @@
+import {CLEAR_ERRORS, GET_ERRORS} from "./constants";
+
+// RETURN ERRORS
+export const returnErrors = (msg, status, id = null) => {
+    return {
+        type: GET_ERRORS,
+        payload: {msg, status, id}
+    }
+};
+
+// CLEAR ERRORS
+export const clearErrors = () => {
+    return {
+        type: CLEAR_ERRORS
+    }
+};
---
+++ client/src/reducers/caseReducer.js
@@ -0,0 +1,30 @@
+import {
+    CASE_UPDATING,
+    ADD_CASE,
+    CASES_LOADING,
+    CASES_LOADED, CASE_UPDATED
+} from "../actions/constants";
+
+const initialState = {
+    isLoading: false,
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case CASES_LOADING:
+        case CASE_UPDATING:
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case CASES_LOADED:
+        case CASE_UPDATED:
+        case ADD_CASE:
+            return {
+                ...state,
+                isLoading: false,
+            };
+        default:
+            return state;
+    }
+}
