b3fad5b6c911ad48ce9e73a903bc1e56ba616e98
Damir Medakovic <damir0096@hotmail.com>
2019-10-22 21:02:49 +0200

    post user unit test

--- api/test/post.js
+++ api/test/post.js
@@ -2,10 +2,31 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 
-const app = require('../routes/caseRouter');
+const app = require('../routes/userRouter');
 
 
 
 
+describe('POST /register', () => {
+   
+
+    it('OK, creating a new user works', (done) => {
+
+        request(app).post('/register').send({
+            name: "Test Tester",
+            email: "test@email.com",
+            password: "abc123"
+        })
+        .then((res) => {
+            const body = res.body;
+            expect(body).to.contain.property('name');
+            expect(body).to.contain.property('email');
+            expect(body).to.contain.property('text');
+            done();
+        })
+        .catch((err) => done(err));
+
+    })
 
 
+})
