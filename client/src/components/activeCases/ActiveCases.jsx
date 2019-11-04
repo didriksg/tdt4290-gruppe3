7222591ca38481a670f091a00131cdc694f7f063
Thea Tokstad <theatok@stud.ntnu.no>
2019-11-04 19:56:02 +0100

    merge fix

--- client/src/components/activeCases/ActiveCases.jsx
+++ client/src/components/activeCases/ActiveCases.jsx
@@ -1,27 +1,8 @@
 import React, {Component} from 'react';
-
 import DistrictFilterButton from "./DistrictFilterButton";
-import {Link} from 'react-router-dom';
-
-import {AppBar, 
-    Toolbar,
-    Grid,
-    Button,
-    Typography, 
-    Card   
-  } from '@material-ui/core';
-import LogoutButton from "../LogoutButton/LogoutButton";
 import OverviewBoard from "../overviewBoard/OverviewBoard";
 import Header from "../header/Header";
 
-
-const style = {
-    persona: {marginTop: 10, marginBottom: 10},
-    personaDescription: {marginBottom: 10},
-    buttons: {marginTop: 10, marginBottom: 10},
-    datatable: {marginTop: 10, marginBottom: 10},
-    mainCard: {marginTop: 5, marginBottom: 5}
-};
 
 class ActiveCases extends Component {
     constructor(props) {
