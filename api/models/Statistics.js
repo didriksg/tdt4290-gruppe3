a0b5447b68abd996d809235bbb310d0a4c326619
Thea Tokstad <33182871+theatok@users.noreply.github.com>
2019-11-04 19:42:56 +0100

    Revert "Feat/district-filterbutton (#20)" (#21)
    This reverts commit 7d19fd203a7b93e55d0435c6dab08d550faafc36.

--- api/models/Statistics.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const StatisticsSchema = new Schema({
-
-});
-
-module.exports = Statistics = mongoose.model('statistics', StatisticsSchema);
